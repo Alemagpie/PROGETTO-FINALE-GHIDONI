8b133387ccacc4d5adda5363744ea374595fe3f4
mer8dado <davidemerotto1@gmail.com>
2025-01-05 18:32:19 +0100

    Sovrascrittura operatore + per Time

--- include/Time.h
+++ include/Time.h
@@ -7,10 +7,35 @@ class Time{
         Time(unsigned int ora, unsigned int minuti);
         unsigned int getHour();
         unsigned int getMinutes();
-    
+
     //Data Membri
     private:
         unsigned int hour;
         unsigned int minutes;
 
+}   
+
+//HELPER FUNCTION
+
+Time& operator +(Time& time1, Time& time2){
+
+    unsigned int finalHour = time1.getHour() + time2.getHour();
+    unsigned int finalMinute = time1.getMinutes() + time2.getMinutes();
+
+    if(finalMinute>=60){
+        finalMinute = finalMinute % 60;
+        finalHour++;
+    }
+
+    //caso termine giornata
+    if(finalHour >=24){
+
+        finalHour = 0;
+        finalMinute = 0;
+    }
+
+    //creo oggetto time
+    Time return_time(finalHour,finalMinute);
+
+    return return_time;
 }
--- src/AutomaticDevices.cpp
+++ src/AutomaticDevices.cpp
@@ -1,6 +1,7 @@
 
 
 #include "../include/AutomaticDevices.h"
+#include "../include/Time.h"
 
 AutomaticDevices:: AutomaticDevices(String name, const int id, double wattProduction, bool status, Time current_time, Time time_duration)
                     : this.name{name}, this.id{id}, this.wattProduction{wattProduction}, this.status{status}, stop_time{current_time+time_duration}{
