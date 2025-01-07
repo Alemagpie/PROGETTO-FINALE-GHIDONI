51f28740f15486151c85cb3aa9bf3e946242caad
Riccardo <ricky.ferrazzo@gmail.com>
2025-01-07 22:17:20 +0100

    WIP di CMake e Parsing

--- src/DeviceManager.cpp
+++ src/DeviceManager.cpp
@@ -14,6 +14,30 @@ void DeviceManager::addDevice(Device& d) {
     //aggiungi entry con (chiave key e valore d) alla multimappa dei device attivi
 }
 
+enum firstCommand{
+    baseValue = 0,set, rm, show, reset
+};
+
+firstCommand firstToSwitch(std::string& const command){
+    if( command == "set") return firstCommand::set;
+    if( command == "rm") return firstCommand::rm;
+    if( command == "show") return firstCommand::show;
+    if( command == "reset") return firstCommand::reset;
+    return firstCommand::baseValue;
+}
+
+enum resetCommand{
+    baseValue=0, time, timers, all
+};
+
+resetCommand resetToSwitch(std::string& const command){
+    if( command == "time") return resetCommand::time;
+    if( command == "timers") return resetCommand::timers;
+    if( command == "all") return resetCommand::all;
+    return resetCommand::baseValue;
+}
+
+
 void DeviceManager::parseInput(std::string command){
     currentDeviceEndTime = currentTime;
     //stream per fare il parsing del comando
@@ -21,60 +45,37 @@ void DeviceManager::parseInput(std::string command){
     std::string word;
     std::getline(iss, word, ' ');
     //controllo della prima parola
-    if(word == "set"){
-        //controllo della seconda parola
-        if(std::getline(iss, word, ' ')){
-            
-        }
+    switch(firstToSwitch(word)){
+        case firstCommand::set:
+            break;
 
+        case firstCommand::rm:
+            break;
 
+        case firstCommand::show:
+            break;
 
+        case firstCommand::reset:
+             if(std::getline(iss, word, ' ')){
+                switch(resetToSwitch(word)){
+                    case firstCommand::set:
+                        break;
+                    case firstCommand::rm:
+                        break;
+                    case firstCommand::show:
+                        break;
+                    default:
+                        std::cout<<"Comando non riconosciuto. Riprovare." << std::endl;
+                        break;
+                }
+             }
+            break;
 
-     } else if (word == "rm") {
-        //controllo della seconda parola
-        if(std::getline(iss, word, ' ')){
-            
-        } else {
+        default:
             std::cout<<"Comando non riconosciuto. Riprovare." << std::endl;
-        }
-
-
-
-
-    } else if (word == "show") {
-        //controllo della seconda parola
-        if(std::getline(iss, word, ' ')){
-
-        } else {
-            std::cout<<"Comando non riconosciuto. Riprovare." << std::endl;
-        }
-
-
-
-
-    } else if (word == "reset") {
-        //controllo della seconda parola
-        if(std::getline(iss, word, ' ')){
-            if(word == "time"){
-                currentTime.setTime(0,0);
-                activeDevices.clear();
-                //Forse bisogna fare qualcosa con gli async
-            } else if (word == "timers") {
-                asyncDevices.clear();
-            } else if (word == "all") {
-                currentTime.setTime(0,0);
-                activeDevices.clear();
-                asyncDevices.clear();
-            } else {
-                std::cout<<"Comando non riconosciuto. Riprovare." << std::endl;
-            }
-            
-        }
-
-    } else {
-        std::cout<<"Comando non riconosciuto. Riprovare." << std::endl;
-    }    
-
+            break;
+    }
+    
 }
 
 void DeviceManager::setTime(Time& newTime) {
